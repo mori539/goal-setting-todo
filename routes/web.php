88806b037d41cc6dd75dc541c6fb79f0ba82ca8f
mori539 <mori.work0118@gmail.com>
2025-12-22 11:36:27 +0900

    ルーティングの整理とコメント付与

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,30 @@
 <?php
 
 use Livewire\Volt\Volt;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Goals\Index as GoalsIndex;
 use App\Livewire\MainTasks\Index as MainTasksIndex;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    // ログイン済みなら、目標一覧へ転送
+    if (Auth::check()) {
+        return redirect()->route('goals.index');
+    }
+    // 未ログインなら、ログインページへ転送
+    return redirect()->route('login');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
 Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
 
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+    // 現状はいらないが、ユーザー設定変更画面を作る場合に必要になりそうなため、コメントアウトしておく
+    // Route::redirect('settings', 'settings/profile');
+
+    // 現状はいらないが、ユーザー設定変更画面を作る場合に必要になりそうなため、コメントアウトしておく
+    // Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    // Volt::route('settings/password', 'settings.password')->name('settings.password');
+    // Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 
     // 目標一覧画面
     Route::get('/goals', GoalsIndex::class)->name('goals.index');
